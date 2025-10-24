37a8bba00b1da84f29cd0ec1f1da42b0a1480156
Атай Эстебесов <atajestebesov@MacBook-Air-Ataj.local>
2025-10-24 21:06:51 +0600

    Friday commit

---
+++ secondlesson.py
@@ -0,0 +1,28 @@
+'''
+3-кавычки помогают для написания большого текста вроде README 
+kjhhgvghkml,jbhvgvbhjnk,klmjvfxdxfgnbjkjgfxszsxcghnml.lmnhcxz`zxdfghjm,..,m
+'''
+# Изучаем списки
+#products = ['milk', 'bread', 'eggs']
+#print(products[1])
+#list = ['список', 34, 656.4, 7==6, 6 != 5, "Жизнь боль", "Я шучу", 65/76, 3.14159265358979832795]
+#print(f'У меня отсутсвует фантазия, поэтому мой 1 элемент это {list[0]}, а остальные элементы логически не связаны. Например: {list[1]}, {list[2]}, {list[5]} и т.д.')
+
+#m = ['knife', 'gun', 'cox']
+#question = input("Что вы хотите купить? ")
+#print(f"я хочу купить {question}")
+#question1 = input("Сколько это стоит?") 
+#print(f"цена {question} = {question1}")
+
+#price = input('Enter the price ')
+#print('Change is ', 500 - float(price))
+
+print(2*(3-1), (5**2-1**2), (0.3*(4-1)), (91-1)/(2+1), sep = '\n', end = 'жизнь боль')
+#print(5**2-1**2)
+#print(0.3*(4-1))
+#print((91-1)/(2+1))
+#print((5*4/3)**1/2)
+#print(1/3+5*0.2*0.001/91)
+#print((40-(4.25*7.08+6.768/0.75))*2050)
+#print((0.16*(3.2-3/40)+2+3/11*4.125/(3*4+3))/((6*5+1)*0.3-0.3*4.5+1/3*0.3))
+print(0.3*3)
