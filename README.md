044908f98ff3994a032964a24b25b26f3d732542
Атай Эстебесов <atajestebesov@MacBook-Air-Ataj.local>
2025-10-24 18:33:08 +0600

    Первая редакция README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-I'm starting to learn Python.
-BTW not the first time
+# Python
+## I'm starting to learn Python. BTW not the first time. Yes, I do unterstand how useless my README is.
+## Content:
++ *Requirements*
++ *Link to *
+
---
+++ firstlesson.py
@@ -0,0 +1,11 @@
+#num = 17.7
+#num1 = 25
+#info = "хахахахаах"
+#print(type(num), type(num1), type(info))
+#num = "3"
+#num1 = 10
+#dif = num1 * num
+#print(dif)
+прозвище = 'чикатило'
+num = 13
+print(f"{прозвище} разрежет тебя на {num} кусочков и съест")
