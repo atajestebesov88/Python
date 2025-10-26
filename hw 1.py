a6bb881d890521e1227d8545a8bf483040a42693
Атай Эстебесов <atajestebesov@MacBook-Air-Ataj.local>
2025-10-26 14:45:57 +0600

    1 дз

---
+++ hw 1.py
@@ -0,0 +1,61 @@
+#Task1
+#num1 = 23212
+#num2 = 63524
+#print(num1+num2)
+
+#Task2
+#num1 = 65
+#num2 = 3
+#num2 = num1//num2
+#print(num2)
+
+#Task3
+#num1 = 876
+#num2 = 76
+#num3 = num1 % num2
+#print(num3)
+
+#Task4
+#num1 = 56.7
+#num2 = 8.4
+#num3 = 6.9
+#num4 = float(num1*num2/num3)
+#print(num4)
+
+#Task5
+#num = float(7)
+#print((num**3)/2)
+
+#Task6
+#number1 = 56.75
+#number2 = 7.87
+#number6 = int(number1-number2)
+#print(number6)
+
+#Task7
+#number1 = 634788.87
+#number2 = 7656.7
+#num7 = int(number1-number2+1)
+#print(num7)
+
+#Task8
+#a = 3
+#b = 4
+#c = (a**2+b**2)**(1/2)
+#print(int(c))
+
+#Task9
+#pos_num = 98765
+#neg_num = -877654
+#print(abs(pos_num + neg_num))
+
+#Task10
+temp = float(345)
+celcium = ((temp - 32)/(5*9))
+print(celcium)
+
+#Task15
+#percent = 2.5
+#number_of_percent = 5.75
+#whole_percent = (100*number_of_percent/percent)
+#print(whole_percent)
